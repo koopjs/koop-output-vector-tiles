93bf09b872e337b0769b2cc7281d63318f13ca87
Malcolm Meyer <malcolmtmeyer@gmail.com>
2019-05-30 09:11:24 -0400

    update for pull request

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const geojsonVT = require('geojson-vt')
 const vtpbf = require('vt-pbf')
 const Logger = require('koop-logger')
 const Winnow = require('winnow')
-const zlib = require('zlib')
-
+const config = require('config')
+const maxAge = config.vectorTiles.maxAge || 3600
 const log = new Logger()
 
-const tiles = new Array();
+const tilesSetCache = [];
 
 function VectorTiles () {}
 VectorTiles.version = require('./package.json').version
@@ -36,19 +36,26 @@ VectorTiles.prototype.serve = function (req, res) {
     // id for the tile layer, assumes the id is already sanitzed no spaces etc
     const id = req.params.id;
     const start = Date.now()
-    if (!tiles[id]) {
+    if (!tilesSetCache[id]) {
       const indexStart = Date.now();
       log.debug(`${id} waiting for geojsonvt to index tiles`)
-      tiles[id] = geojsonVT(data, {
-        maxZoom: 17,
-        indexMaxZoom: 5,
-        buffer: 2048,
-        tolerance: 2
+      tilesSetCache[id] = geojsonVT(data, {
+        //these are the original defaults from geojson-vt
+        maxZoom: config.vectorTiles.geojsonVT.maxZoom || 14,  // max zoom to preserve detail on; can't be higher than 24
+        tolerance: config.vectorTiles.geojsonVT.tolerance || 3, // simplification tolerance (higher means simpler)
+        extent: config.vectorTiles.geojsonVT.extent || 4096, // tile extent (both width and height)
+        buffer: config.vectorTiles.geojsonVT.buffer || 64,   // 64 is the original default, using a higher number like 512, 1024 or 2048 gets rid of some geojson artifacts but increases the tilesSetCache size // tile buffer on each side
+        debug: config.vectorTiles.geojsonVT.debug || 0,     // logging level (0 to disable, 1 or 2)
+        lineMetrics: config.vectorTiles.geojsonVT.lineMetrics || false, // whether to enable line metrics tracking for LineString/MultiLineString features
+        promoteId: config.vectorTiles.geojsonVT.promoteId || null,    // name of a feature property to promote to feature.id. Cannot be used with `generateId`
+        generateId: config.vectorTiles.geojsonVT.generateId || false,  // whether to generate feature ids. Cannot be used with `promoteId`
+        indexMaxZoom: config.vectorTiles.geojsonVT.indexMaxZoom || 5,       // max zoom in the initial tile index
+        indexMaxPoints: config.vectorTiles.geojsonVT.indexMaxPoints || 100000 // max number of points per tile in the index
       })
       const indexEnd = (Date.now() - indexStart) / 1000;
       log.debug(`${id} tile indexing complete in ${indexEnd} milliseconds`)
     }
-    const tile = tiles[id].getTile(z,x,y)
+    const tile = tilesSetCache[id].getTile(z,x,y)
     if (!tile) {
       res.set(204)
       return res.send()
@@ -60,14 +67,11 @@ VectorTiles.prototype.serve = function (req, res) {
     const duration = (Date.now() - start) / 1000
     log.debug(`${id},output=vector-tiles tile=${z},${x},${y} features=${data.features.length} duration=${duration}`)
     res.set({
-      'Cache-Control': 'public, max-age:3600',
-      'Content-Encoding': 'gzip'
+      // 'Content-Type': 'application/x-protobuf', //issued a pull request to add x-protobuf compressible:true to mime-db, until then need application/json or other compressible format mime type to have compression use gzip on these
+      'Content-Type': 'application/json',
+      'Cache-Control': 'public, max-age:' + maxAge //not sure if this is needed as eventually the cache will be handled by koop memory or in nginx, but leaving it in for now
     })
-
-    //zlib.gzip enables a 4x decrease in data size sent with no noticeable performance decrease
-    zlib.gzip(pbf, function(_, result) {
-      res.send(result);
-    })
+    res.send(pbf);
   })
 }
 // TODO change the "tiles" to reflect the id above and optional query parameters
