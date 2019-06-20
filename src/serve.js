a9610ad91f916deea7f3ff108e2d90dc3a5fa75a
Rich <rich.gwozdz@gmail.com>
2019-06-20 11:19:26 -0700

    Change config namespace.

--- src/serve.js
+++ src/serve.js
@@ -8,28 +8,31 @@ const log = new Logger()
 const Utils = require('./utils')
 const TileSetsCache = require('./tile-sets-cache')
 
-const useTileSetCache = _.get(config, 'vectorTiles.cache', true)
+const useTileSetCache = _.get(config, 'koopOutputVectorTiles.cache', true)
 
 // Tile response headers
 const responseHeaders = {
-  'Content-Type': _.get(config, 'vectorTiles.contentType', 'application/x-protobuf'),
-  'Cache-Control': `public, max-age: ${_.get(config, 'vectorTiles.maxAge', 3600)}`
+  'Content-Type': _.get(config, 'koopOutputVectorTiles.contentType', 'application/x-protobuf'),
+  'Cache-Control': `public, max-age: ${_.get(config, 'koopOutputVectorTiles.maxAge', 3600)}`
 }
 
-const tileSetsCache = new TileSetsCache({ expires: _.get(config, 'vectorTiles.cacheExpiration') })
+const tileSetsCache = new TileSetsCache({ ttl: _.get(config, 'koopOutputVectorTiles.cacheExpiration') })
 
 function getPbfTile (key, data, z, y, x, options = {}) {
   let tileSet
 
+  // Use in-memory tileset cache if configured, otherwise generate tile set on-the-fly
   if (useTileSetCache) {
     tileSet = tileSetsCache.retrieve(key)
     if (!tileSet) tileSet = tileSetsCache.upsert(key, data)
   } else tileSet = geojsonVT(data, tileSetConfig)
 
+  // Get tile from tile set
   const tile = tileSet.getTile(z, x, y)
 
   if (!tile) return
 
+  // Make pbf
   return Buffer.from(vtpbf.fromGeojsonVt({
     [key]: tile
   }))
